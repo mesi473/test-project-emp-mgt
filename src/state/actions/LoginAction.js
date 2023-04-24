ff5142834348e1516b1c5d8c68eb009c1a926a53
mesi473 <meseretkifle2@gmail.com>
2023-04-25 00:02:20 +0300

    acount  redux action and reducers

---
+++ src/state/actions/LoginAction.js
@@ -0,0 +1,71 @@
+import * as actionTypes from "../actionTypes";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import FirebaseApp from "../../auth/firebaseConfig";
+
+export const LoginAction = (userData) => async (dispatch) => {
+  const auth = getAuth(FirebaseApp);
+  dispatch({
+    type: actionTypes.LOGIN_START,
+  });
+
+  signInWithEmailAndPassword(auth, userData.email, userData.password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      if (user) {
+        dispatch({
+          type: actionTypes.LOGIN_SUCCESS,
+          payload: {
+            user: user,
+            password: userData.password,
+          },
+        });
+        localStorage.setItem("token", user.accessToken);
+        localStorage.setItem("email", user.email);
+      }
+    })
+    .catch((err) => {
+      dispatch({
+        type: actionTypes.LOGIN_ERROR,
+        payload: err,
+      });
+    });
+};
+export const LoginWithGoogleAction = () => async (dispatch) => {
+  const auth = getAuth(FirebaseApp);
+  dispatch({
+    type: actionTypes.LOGIN_START,
+  });
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+      if (user) {
+        dispatch({
+          type: actionTypes.LOGIN_SUCCESS,
+          payload: {
+            user: user,
+          },
+        });
+        localStorage.setItem("token", user.accessToken);
+        localStorage.setItem("email", user.email);
+      }
+    })
+    .catch((error) => {
+      dispatch({
+        type: actionTypes.LOGIN_ERROR,
+        payload: error,
+      });
+    });
+};
+
+export const ClearLoginAction = () => async (dispatch) => {
+  dispatch({
+    type: actionTypes.CLEAR_LOGIN_STATE,
+  });
+};
---
+++ src/state/reducers/AccountReducer.js
@@ -0,0 +1,46 @@
+
+import * as actionTypes from '../actionTypes';
+
+const initialState = {
+    token: null,
+    email: null,
+    error_message: null,
+    error_code:null,
+    message: null,
+    loading:false,
+}
+export const AccountReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.LOGIN_SUCCESS:
+                return {
+                    ...state,
+                    token:action.payload?.user?.accessToken,
+                    email:action.payload?.user?.email,
+                    password:action.payload?.password,
+                    loading:false,
+                }
+        case actionTypes.LOGIN_START:
+            return{
+                ...state,
+                loading:true,
+            }
+        case actionTypes.LOGIN_ERROR:
+            return {
+                ...state,
+                error_code: action.payload?.code,
+                error_message:action.payload?.message,
+                loading:false
+            }
+        case actionTypes.CLEAR_LOGIN_STATE:
+            return {
+                error_message:null,
+                error_code:null,
+                loading:false,
+                message:null
+            }
+        default:
+            return {
+                ...state
+            }
+    }
+}
